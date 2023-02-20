a34626781c77cd06eaafef0f772afbd3786b80e9
khokhoa <dbspgnl@naver.com>
2023-02-20 22:11:28 +0900

    + 230220_3~4

---
+++ 연습/2302/230220_4.py
@@ -0,0 +1,47 @@
+"""
+# 세로 # 가로
+# 0벽 1길
+6 5
+1 1 0 1 1
+0 1 1 0 0
+0 0 0 0 0
+1 0 1 1 1
+0 0 1 1 1
+0 0 1 1 1
+= 4
+9
+# 길 그룹 수
+# 가장 긴 길 크기
+"""
+# bfs는 힙을 사용한다.
+import sys
+input = sys.stdin.readline
+n,m = map(int, input().split())
+map = [list(map(int, input().split())) for _ in range(n)]
+chk = [[False]*m for _ in range(n)]
+dy = [0,1,0,-1]
+dx = [1,0,-1,0]
+def bfs(y,x):
+    q = [(y,x)]
+    rs = 0
+    while q:
+        rs +=1
+        ey, ex = q.pop()
+        for k in range(4):
+            ny = ey + dy[k]
+            nx = ex + dx[k]
+            if 0<=ny<n and 0<=nx<m:
+                if map[ny][nx] == 1 and chk[ny][nx] == False:
+                    chk[ny][nx] = True
+                    q.append((ny,nx))
+    return rs
+count = 0
+maxv = 0
+for j in range(n):
+    for i in range(m):
+        if map[j][i] == 1 and chk[j][i] == False:
+            chk[j][i] = True
+            count +=1
+            maxv = max(maxv, bfs(j,i))
+print(count)
+print(maxv)
