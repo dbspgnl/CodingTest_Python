917149f9964bd5c33f8a03f4a4ff79cd8b131241
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2023-02-23 08:40:03 +0900

    + 230223_1~4

---
+++ 연습/2302/230223_3.py
@@ -0,0 +1,46 @@
+"""
+# 세로 # 가로
+# 0벽 1길
+6 5
+1 1 0 1 1
+0 1 1 0 0
+0 0 0 0 0
+1 0 1 1 1
+0 0 1 1 1
+0 0 1 1 1
+= 4
+9
+# 길 그룹 수
+# 가장 긴 길 크기
+"""
+import sys
+input = sys.stdin.readline
+n,m = map(int, input().split())
+map = [list(map(int, input().split())) for _ in range(n)]
+chk = [[False]*m for _ in range(n)]
+dy = [0,1,0,-1]
+dx = [1,0,-1,0]
+def bfs(y,x):
+    q = [(y,x)]
+    rs = 0
+    while q:
+        ey, ex = q.pop()
+        rs += 1
+        for k in range(4):
+            ny = ey + dy[k]
+            nx = ex + dx[k]
+            if 0<=ny<n and 0<=nx<m:
+                if map[ny][nx] == 1 and chk[ny][nx] == False:
+                    chk[ny][nx] = True
+                    q.append((ny,nx))
+    return rs
+cnt = 0
+maxv = 0
+for j in range(n):
+    for i in range(m):
+        if map[j][i] == 1 and chk[j][i] == False:
+            chk[j][i] = True
+            cnt +=1
+            maxv = max(maxv, bfs(j,i))
+print(cnt)
+print(maxv)
