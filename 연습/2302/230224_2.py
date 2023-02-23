44457b1a1f863c91521e3eae7378ffade0aa19b7
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2023-02-24 08:52:05 +0900

    + 230224_1~2

---
+++ 연습/2302/230224_2.py
@@ -0,0 +1,45 @@
+"""
+# 노드 수 / 간선 수
+# 시작 노드
+# 인접 리스트
+5 6
+1
+5 1 1
+1 2 2
+1 3 3
+2 3 4
+2 4 5
+3 4 6
+= 0
+2
+3
+7
+INF
+# 시작점에서 각 노드까지의 거리
+"""
+import sys
+import heapq
+inf = sys.maxsize
+input = sys.stdin.readline
+v,e = map(int, input().split())
+k = int(input())
+eList = [[] for _ in range(v+1)]
+dist = [inf] * (v+1)
+
+for _ in range(e):
+    a,b,w = map(int, input().split())
+    eList[a].append([w,b])
+
+heap = [[0,k]]
+dist[k] = 0
+while heap:
+    ew, ev = heapq.heappop(heap)
+    if ew != dist[ev]: continue
+    for nw, nv in eList[ev]:
+        if dist[nv] > nw + ew:
+            dist[nv] = nw + ew
+            heapq.heappush(heap, [dist[nv],nv])
+
+for i in range(1, v+1):
+    if dist[i] == inf: print("INF")
+    else: print(dist[i])
