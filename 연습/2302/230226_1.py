13a2c207c84f89e55127e31aec0abb67bfa3e4d0
khokhoa <dbspgnl@naver.com>
2023-02-26 15:21:44 +0900

    + 230226_1~6

---
+++ 연습/2302/230226_1.py
@@ -0,0 +1,40 @@
+"""
+# 노드 수 / 엣지 수
+# 1번노드가 2번노드에 몇 가중치로? (양방향)
+# 시작은 1로
+3 3
+1 2 1
+2 3 2
+1 3 3
+= 3
+# 모든 노드를 이은 가중치의 합은 3
+4 4
+1 2 3
+2 3 4
+1 4 5
+4 2 3
+= 10
+"""
+import sys
+import heapq
+input = sys.stdin.readline
+v,e = map(int, input().split())
+eList = [[] for _ in range(v+1)]
+chk = [False] * (v+1)
+
+for _ in range(e):
+    a,b,w = map(int, input().split())
+    eList[a].append([w,b])
+    eList[b].append([w,a])
+
+heap = [[0,1]]
+rs = 0
+while heap:
+    ew, ev = heapq.heappop(heap)
+    if chk[ev] == False:
+        chk[ev] = True
+        rs +=ew
+        for nList in eList[ev]:
+            if chk[nList[1]] == False:
+                heapq.heappush(heap, nList)
+print(rs)
---
+++ 연습/2302/230226_2.py
@@ -0,0 +1,43 @@
+"""
+# 노드 수 / 간선 수
+# 시작 노드
+# 인접 리스트
+5 6
+1
+5 1 1
+1 2 2
+1 3 3
+2 3 4
+2 4 5
+3 4 6
+= 0
+2
+3
+7
+INF
+# 시작점에서 각 노드까지의 거리
+"""
+import sys
+import heapq
+input = sys.stdin.readline
+inf = sys.maxsize
+v,e = map(int, input().split())
+k = int(input())
+eList = [[] for _ in range(v+1)]
+dist = [inf] * (v+1)
+
+for _ in range(e):
+    a,b,w = map(int, input().split())
+    eList[a].append([w,b])
+
+heap = [[0,1]]
+while heap:
+    ew, ev = heapq.heappop(heap)
+    if dist[ev] !=  ew: continue
+    for nw, nv in eList[ev]:
+        if dist[nv] > ew + nw:
+            dist[nv] = ew + nw
+            heapq.heappush(heap, [dist[nv],nv])
+for i in range(1, v+1):
+    if dist[i] == inf: print("INF")
+    else: print(dist[i])
