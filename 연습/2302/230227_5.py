9c2078311dac08db7377e479ce6f0aa1ded8c989
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2023-02-27 18:32:45 +0900

    + 230227_5~7

---
+++ 연습/2302/230227_5.py
@@ -0,0 +1,40 @@
+"""
+# 배열 길이
+# 주어진 배열
+# 찾을 수
+# 찾을 수 배열
+5
+4 1 5 2 3
+5
+1 3 7 9 5
+= 1
+1
+0
+0
+1
+# 찾을 수 만큼 체크하여 찾을 수가 있으면 1, 없으면 0
+"""
+a = int(input())
+aList = list(map(int, input().split()))
+aList.sort()
+t = int(input())
+tList = list(map(int, input().split()))
+
+for i in range(t):
+    start = 0
+    end = len(aList)-1
+    find = False
+    target = tList[i]
+    while start <= end: # 넘어서면 종료
+        mi = int((start+end)//2)
+        mv = aList[mi]
+        if mv > target:
+            end = mi-1
+        elif mv < target:
+            start = mi+1
+        else:
+            find = True
+            break
+    if find: print(1)
+    else: print(0)
+
