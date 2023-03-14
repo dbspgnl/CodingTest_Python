78b2eceeb01f6917d5be45ae35e3612daf9c25dd
khokhoa <dbspgnl@naver.com>
2023-03-14 20:48:42 +0900

    + 0314_9~10

---
+++ 연습/2303_3/0314_10.py
@@ -0,0 +1,36 @@
+"""
+#4P2 팩토리얼을 구하는 N,M을 입력받음
+4 2
+= 1 2
+1 3
+1 4
+2 1
+2 3
+2 4
+3 1
+3 2
+3 4
+4 1
+4 2
+4 3
+#모든 경우의 수를 처리
+"""
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+chk = [False] * (n+1)
+
+rs = []
+def recur(num):
+    if num == m:
+        print(' '.join(map(str, rs)))
+        return
+    for i in range(1, n+1):
+        if chk[i] == False:
+            chk[i] = True
+            rs.append(i)
+            recur(num+1)
+            chk[i] = False
+            rs.pop()
+recur(0)
