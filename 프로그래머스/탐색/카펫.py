6ee7cb3f5a608b09a858d5a0ac9f01f3278c43dc
khokhoa <dbspgnl@naver.com>
2023-04-17 22:55:20 +0900

    + 탐색/카펫

--- 프로그래머스/탐색/카펫.py
+++ 프로그래머스/탐색/카펫.py
@@ -1,9 +1,19 @@
+# brown + yellow = x*y
+# x >= y
+# yello가 같더라도 brown에 따라 가로가 달라짐
+#
 def solution(brown, yellow):
-    # brown + yellow = x*y
-    # x >= y
-    # yello가 같더라도 brown에 따라 가로가 달라짐
-
     answer = []
+    x = 0
+    y = 0
+    for i in range(1, yellow+1):
+        if yellow % i == 0: # 가로x세로 조건
+            x = int(yellow/i)
+            y = i
+            if (2*x) + (2*y) + 4 == brown: # 옐로우 가로 세로 + 모서리
+                answer.append(x+2) # 브라운은 옐로보다 2칸 더 길다
+                answer.append(y+2)
+                return answer
     return answer
 
 
