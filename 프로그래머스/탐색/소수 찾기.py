0dd2b65a2ce8438bc6f816409de0cfc87e910aca
swpark@yellotail.com <swpark@yellotail.com>
2023-04-17 18:31:00 +0900

    + 탐색/소수 찾기 + 탐색/카펫(작성중)

--- 프로그래머스/탐색/소수 찾기.py
+++ 프로그래머스/탐색/소수 찾기.py
@@ -1,24 +1,37 @@
 from itertools import permutations
-import math
-
-def primenumber(x):
-    for i in range (2, int(math.sqrt(x) + 1)):	
-    	if x % i == 0:
-            return False
-    return True
 
 def solution(numbers):
-    per = list(permutations(list(numbers)))
-    num_list = [''.join(p) for p in per]
-    num_list += list(numbers)
-    num_list = set(num_list)
-    num_list.discard("0")
-    num_list.discard("1")
-    count = 0
-    for num in num_list:
-        i = int(num)
-        if primenumber(i): count +=1
-    return count
+    nums = [n for n in numbers]
+    per = []
+    for i in range(1, len(numbers) + 1): # 모든 경우 순열로
+        per += list(permutations(nums, i)) # [('1',), ('7',), ('1', '7'), ('7', '1')]
+    new_nums = [int(("").join(p)) for p in per] # [71, 17, 1, 7]
+    new_nums = set(new_nums)
+    answer = []
+    for n in new_nums:
+        if n < 2:  # 0,1은 소수 아님
+            continue
+        check = True
+        for i in range(2, int(n ** 0.5) + 1):  # n의 제곱근까지만
+            if n % i == 0:  # 나눠 떨어지면 소수아님
+                check = False
+                break
+        if check:
+            answer.append(n)
+    return len(answer)
 
 print(solution("17")) #3
 print(solution("011")) #2
+print(solution("1111111")) #1
+
+"""
+from itertools import permutations
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
+"""
---
+++ 프로그래머스/탐색/카펫.py
@@ -0,0 +1,15 @@
+def solution(brown, yellow):
+    # brown + yellow = x*y
+    # x >= y
+    # yello가 같더라도 brown에 따라 가로가 달라짐
+
+    answer = []
+    return answer
+
+
+print(solution(10,2)) # [4, 3]
+print(solution(8,1)) # [3, 3]
+print(solution(24,24)) # [8, 6]
+print(solution(12,3)) # [5, 3]
+print(solution(16,9)) # [5, 5]
+print(solution(24,9)) # [11, 3]
