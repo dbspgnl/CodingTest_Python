435787c695d07b7fd410e264f24af1dd44942fa2
swpark@yellotail.com <swpark@yellotail.com>
2023-05-19 08:30:21 +0900

    + 연습/0519(1)

---
+++ 연습/2305/0519_게임 맵 최단거리.py
@@ -0,0 +1,28 @@
+# 0은 벽이고, 1은 길이다. 가장 짧은 길을 찾고, 없으면 -1
+# 시작점은 0,0 끝점은 반대편
+from collections import deque
+def solution(maps):
+    n = len(maps[0]) # x
+    m = len(maps) # y
+    count = 1
+    queue = deque()
+    queue.append([0,0,count]) # 초기화
+    maps[0][0] = 0 # 시작위치 방문처리
+    dx = [-1,0,1,0]
+    dy = [0,-1,0,1]
+    while queue:
+        x,y,count = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx == m-1 and ny == n-1:
+                return count+1 # 마지막 위치 도착
+            if 0 <= nx < m and 0 <= ny < n:
+                if maps[nx][ny] != 0: # 방문하지 않았으면
+                    maps[nx][ny] = 0 # 방문 처리
+                    queue.append([nx,ny,count+1])
+    return -1
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]])) # 11
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]])) # -1
+
