4a2bad282899713d9fe1e1356e7e8277cc6a553a
swpark@yellotail.com <swpark@yellotail.com>
2023-07-31 12:56:43 +0900

    + 그래프/가장 먼 노드

---
+++ 프로그래머스/그래프/가장 먼 노드.py
@@ -0,0 +1,22 @@
+from collections import deque
+def solution(n, edge):
+    answer = 0
+    graph = [[] for _ in range(n+1)]
+    dist = [-1] * (n+1)
+    for a,b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+    queue = deque([1])
+    dist[1]=0
+    while queue:
+        now = queue.popleft()
+        for i in graph[now]:
+            if dist[i] == -1: # 방문 유무
+                queue.append(i)
+                dist[i] = dist[now]+1
+    for d in dist: # 최대 거리 갯수
+        if d == max(dist):
+            answer +=1
+    return answer
+
+print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]])) #3
